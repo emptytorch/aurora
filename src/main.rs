fc81b6ad7f2f2f043ff0c9ff024baa432db01247
emptytorch <emptytorch@proton.me>
2025-11-11 22:51:14 +0100

    pretty diagnostics

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
 
 use clap::{Parser, Subcommand};
+
+use crate::diagnostic::{Diagnostic, Level};
 
 mod ast;
 mod diagnostic;
@@ -27,15 +29,33 @@ enum Command {
     },
 }
 
+fn dump_diagnostic(input: &str, path: &Path, diagnostic: &Diagnostic) {
+    let report = &[annotate_snippets::Level::ERROR
+        .primary_title(&diagnostic.message)
+        .element(
+            annotate_snippets::Snippet::source(input)
+                .line_start(1)
+                .path(path.to_string_lossy())
+                // TODO: multiple labels
+                .annotations(diagnostic.labels.iter().map(|label| {
+                    annotate_snippets::AnnotationKind::Primary
+                        .span(label.span.start..label.span.end)
+                        .label(&label.message)
+                })),
+        )];
+    let renderer = annotate_snippets::Renderer::styled()
+        .decor_style(annotate_snippets::renderer::DecorStyle::Unicode);
+    println!("{}", renderer.render(report));
+}
+
 fn main() {
     match Args::parse().cmd {
         Command::Run { path } => {
-            let input = std::fs::read_to_string(path).expect("could not read .au file");
+            let input = std::fs::read_to_string(&path).expect("could not read .au file");
             // TODO: pretty diagnostics
-            match machine::execute(&input) {
-                Ok(_) => {}
-                Err(e) => println!("{:?}", e),
-            }
+            if let Err(diagnostic) = machine::execute(&input) {
+                dump_diagnostic(&input, &path, &diagnostic);
+            };
         }
     }
 }
